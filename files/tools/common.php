da9eebf8d981ad116dd34440469742e7721d4568
kouinkouin <me@kouinkouin.com>
2019-06-03 13:48:58 +0200

    style: format code

--- files/tools/common.php
+++ files/tools/common.php
@@ -1,8 +1,8 @@
 <?php
 
-if ( ! is_file($root_dir . '/config/config.inc.php')) {
+if ( ! is_file($rootPath . '/config/config.inc.php')) {
     echo 'You are not in a Prestashop root directory !', PHP_EOL, 'Bye!', PHP_EOL;
     exit(1);
 }
 
-require_once $root_dir . '/config/config.inc.php';
+require_once $rootPath . '/config/config.inc.php';
